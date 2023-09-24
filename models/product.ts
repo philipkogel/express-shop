936fda5f216ac256fa983a5a0cee4f30d0f65427
philipkogel <philipkogel@yahoo.pl>
2023-09-24 11:55:21 +0200

    feat(*): products id for dynamic details product routes

--- models/product.ts
+++ models/product.ts
@@ -1,6 +1,7 @@
 const path = require('path');
 
 interface IProduct {
+  id: string;
   title: string;
   imageUrl: string;
   description: string;
@@ -14,11 +15,13 @@ const getProductsFromFile = async (): Promise<IProduct[]> => {
 }
 
 module.exports = class Product implements IProduct {
+  id: string;
   title: string;
   imageUrl: string;
   description: string;
   price: number;
   constructor(title: string, imageUrl: string, price: number, description: string,) {
+    this.id = crypto.randomUUID()
     this.title = title;
     this.imageUrl = imageUrl;
     this.description = description;
